effe8b009f0b2198d881eb3b4b995fd4b34d4e22
anishbista <anishbista9235@gmail.com>
2020-08-01 15:32:25 +0545

    first

---
+++ volume of sphere.py
@@ -0,0 +1,8 @@
+print("To calculate the volume of sphere")
+diameter = int(input("Enter the diameter of sphere \n"))
+pi= 3.1415926535897931
+radius = diameter/2
+volume = (4/3)*pi*radius**3
+print("The volume of sphere is \n"+ str(volume))
+
+print("anish")
---
+++ work with string.py
@@ -0,0 +1,55 @@
+from math import *
+#for upper case
+name= "Anish Bista"
+print(name .upper())
+
+#for lower case
+print(name .lower())
+
+#for length of string
+print(len(name))
+
+#to check if string is in upper case
+print(name .isupper())
+
+##to check if string is in lower case
+print(name .islower())
+
+#to check the poistion of string
+print(name .index("i"))
+
+#to get string at specificed position
+print(name [4])
+
+#to replace string with another
+print(name .replace("nish" , "shish"))
+
+print('anish\"bista\"')
+
+#to get reminder
+print(21%4)
+
+#for power
+print(pow(3,5))
+
+#for bigger number
+print(max(3,5,9,2,3,6))
+
+#for small number
+print(min(3,5,9,63,85,1))
+
+#for rounding number
+print(round(3.50))
+
+print(ceil(3.75))
+
+print(floor(3.75))
+
+#for square root
+print(sqrt(49))
+
+
+
+
+
+
