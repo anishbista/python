a7afd1cb29ba3f249ed5e2b95c3c90e2da4bfa17
anishbista <anishbista9235@gmail.com>
2020-08-01 20:23:19 +0545

    random + age

---
+++ age difference.py
@@ -0,0 +1,16 @@
+import random
+import datetime
+now = datetime.datetime.now()
+random_sentences = ["dont talk about future you may die in 2020 ",
+                    "are you from duture", 
+                    "are you mad", 
+                    "this is bullshot man fuck off"]
+year = input("Enter your birth year in AD \n")
+year = int(year)
+if(year > now.year):
+    randInt = random.randint(0, 3)
+    print(random_sentences[randInt])
+else:
+    age = now.year - year
+    print("Your age is " + str(age) + "year old")
+
