70964067c66e27c97bdc196160e169db8bff8f2d
anishbista <anishbista9235@gmail.com>
2020-08-02 16:58:11 +0545

    minor changes

--- age difference.py
+++ age difference.py
@@ -5,7 +5,7 @@ random_sentences = ["dont talk about future you may die in 2020 ",
                     "are you from future", 
                     "are you mad", 
                     "this is bullshit man fuck off"]
-year = input("Enter your birth year in AD \n")
+year = input("Enter your Birth Year in AD \n")
 year = int(year)
 if(year > now.year):
     randInt = random.randint(0, 3)
